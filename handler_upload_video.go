5b399967d80ae7d030a4e456cd5e4908244ec024
Jossany Silva <jossanysilva@CQ57W1RFVG.local>
2025-08-26 17:30:33 -0300

    Replace S3 presigned URLs with CloudFront distribution URLs
    - Remove presigned URL generation functions and related imports
    - Switch video URL format from S3 bucket to CloudFront distribution
    - Simplify video handlers by removing dbVideoToSignedVideo calls
    - Update video upload handler to use cfg.s3CfDistribution instead of cfg.s3Bucket

--- handler_upload_video.go
+++ handler_upload_video.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bytes"
-	"context"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -11,12 +10,9 @@ import (
 	"os"
 	"os/exec"
 	"path"
-	"strings"
-	"time"
 
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
-	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/database"
 	"github.com/google/uuid"
 )
 
@@ -148,7 +144,7 @@ func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request)
 		fmt.Printf("Warning: failed to remove processed file: %v\n", err)
 	}
 
-	url := fmt.Sprintf("%s,%s", cfg.s3Bucket, fileName)
+	url := fmt.Sprintf("%s/%s", cfg.s3CfDistribution, fileName)
 	videoData.VideoURL = &url
 
 	err = cfg.db.UpdateVideo(videoData)
@@ -157,13 +153,7 @@ func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request)
 		return
 	}
 
-	signedVideo, err := cfg.dbVideoToSignedVideo(videoData)
-	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, "Internal Server Error", err)
-		return
-	}
-
-	respondWithJSON(w, http.StatusOK, signedVideo)
+	respondWithJSON(w, http.StatusOK, videoData)
 }
 
 func getVideoAspectRatio(filepath string) (string, error) {
@@ -240,35 +230,4 @@ func processVideoForFastStart(inputFilePath string) (string, error) {
 	}
 
 	return processedFilePath, nil
-}
-
-func generatePresignedUrl(s3Client *s3.Client, bucket, key string, expireTime time.Duration) (string, error) {
-	presignClient := s3.NewPresignClient(s3Client)
-	preSignedHttpRequest, err := presignClient.PresignGetObject(context.TODO(), &s3.GetObjectInput{
-		Bucket: &bucket,
-		Key:    &key,
-	}, s3.WithPresignExpires(expireTime))
-	if err != nil {
-		return "", fmt.Errorf("error generating presigned http request: %v", err)
-	}
-
-	return preSignedHttpRequest.URL, nil
-}
-
-func (cfg *apiConfig) dbVideoToSignedVideo(video database.Video) (database.Video, error) {
-	if video.VideoURL == nil {
-		return database.Video{}, fmt.Errorf("video URL is not set for video ID: %s", video.ID.String())
-	}
-	videoURL := video.VideoURL
-	videoURLSlice := strings.Split(*videoURL, ",")
-	if len(videoURLSlice) < 2 {
-		return database.Video{}, fmt.Errorf("invalid video URL format: expected comma-separated values, got %s: ", *videoURL)
-	}
-	presignedUrl, err := generatePresignedUrl(cfg.s3Client, videoURLSlice[0], videoURLSlice[1], 5*time.Minute)
-	if err != nil {
-		return database.Video{}, err
-	}
-
-	video.VideoURL = &presignedUrl
-	return video, nil
 }
